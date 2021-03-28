5fcccfef8cb36d0ed9b1b711d9379ac05a56e96e
Shin, Seong-il <callmeshin75@gmail.com>
2021-03-28 19:31:08 +0900

    add init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import InitPage from "./routes/Initpage";
+import Main from "./routes/Main";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <Router>
+         <Switch>
+            <Route exact path="/" component={InitPage} />
+            <Route exact path="/main" component={Main} />
+         </Switch>
+      </Router>
+   );
 }
 
 export default App;
